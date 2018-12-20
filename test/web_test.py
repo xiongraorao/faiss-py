d58a9e2496704d31255ae1d5a65ac4fe652aefbd
xiongraorao <xiongraorao@gmail.com>
2018-12-20 14:01:55 +0800

    update logger web_test

--- test/web_test.py
+++ test/web_test.py
@@ -8,21 +8,22 @@ xb[:, 0] += np.arange(nb) / 10000.
 xq = xb[:5]
 
 headers = {'content-type': "application/json"}
+base_url = 'http://192.168.1.6:2344'
 
 def add():
-    url = 'http://localhost:2344/add'
+    url = base_url + '/add'
     data = {'ntotal': nb, 'data': {'ids': list(range(nb)), 'vectors': xb.tolist()}}
     response = requests.post(url, data=json.dumps(data), headers=headers)
     print(response.json())
 
 def search():
-    url = 'http://localhost:2344/search'
+    url = base_url + '/search'
     data = {'qtotal': 5, 'topk':10, 'queries': xq.tolist()}
     response = requests.post(url, data=json.dumps(data), headers=headers)
     print(response.json())
 
 def delete():
-    url = 'http://localhost:2344/del'
+    url = base_url + '/del'
     data = {'ids': list(range(1,7))}
     response = requests.post(url, data=json.dumps(data), headers=headers)
     print(response.json())
